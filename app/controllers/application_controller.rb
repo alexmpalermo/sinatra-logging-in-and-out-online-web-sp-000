5915bb12d3d24da99680f3b273a1e5a9982a9fc4
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-29 21:15:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,12 +11,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params["username"], password: params["password"])
-    if @user
-      session[:user_id] = @user.id
-      redirect '/account'
+   @user = User.find_by(:username => params[:username])
+      if @user != nil && @user.password == params[:password]
+
+
+       session[:user_id] = @user.id
+      redirect to '/account'
     end
-    redirect '/error'
+    erb :error
   end
 
   get '/account' do
