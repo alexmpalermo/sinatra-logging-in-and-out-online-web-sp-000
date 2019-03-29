81daedff44bad19a8ecae15c453d0b35180e33f5
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-29 21:16:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,8 +22,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    
-    erb :error
+     @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
